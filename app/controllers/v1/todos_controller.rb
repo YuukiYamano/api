448f079e81b9d573685d982dfea87d9df9d56e3e
YuukiYamano <yuukiyamano1108@gmail.com>
2023-01-19 22:12:45 +0900

    feat: feat basic func

--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -1,2 +1,30 @@
 class V1::TodosController < ApplicationController
+    def index
+        todos = Todo.all
+        render json:todos
+    end
+
+    def create
+        todo = Todo.new(todo_params)
+        if todo.save
+          render json: todo
+        else
+          render json: todo.errors
+        end
+      end
+
+      def destory
+        todo = Todo.find(params[:id])
+        if todo.destory
+            render json:todo
+        end
+      end
+
+      private
+
+      #setting storng parameter
+      def todo_params
+        params.require(:todo).permit(:title,:user_id)
+        
+      end
 end
